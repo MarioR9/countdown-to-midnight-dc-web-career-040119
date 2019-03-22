042b66f0bd703714a97947296d581ab4fc22c01d
mario rodrigueza <mariorodriguezan@gmail.com>
2019-03-22 00:57:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown(n)
-  counter = 0 
+  counter = 10
   while counter < n
-  puts "#{counter} SECONDS(s)""
+  puts "#{counter} SECONDS(s)"
   conter -= 1
   end
 end
